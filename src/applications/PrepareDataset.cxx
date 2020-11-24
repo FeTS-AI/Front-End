bc1e3c5e701e13d8355da69ca994fee7e3f34430
sarthakpati <sarthak.pati@hotmail.com>
2020-11-24 09:33:19 -0500

    starting putting the cli together

--- src/applications/PrepareDataset.cxx
+++ src/applications/PrepareDataset.cxx
@@ -4,6 +4,12 @@
 
 int main(int argc, char** argv)
 {
+  cbica::CmdParser parser(argc, argv, "PrepareDataset");
+
+  parser.addApplicationDescription("This application calls the BraTSPipeline for all input images and stores the final and intermediate files separately");
+  parser.addRequiredParameter("i", "inputCSV", cbica::Parameter::FILE, "Input CSV file", "Input CSV file which contains paths to structural images", "Headers should be 'PatientID,T1,T1GD,T2,T2FLAIR'");
+  parser.addRequiredParameter("o", "outputDir", cbica::Parameter::DIRECTORY, "Directory", "Output directory for final output", "This will write 2 folders: 'DataForFeTS' and 'DataForQC'", 
+    "Former contains only the files needed for FeTS inference/training and ", "latter contains all intermediate files from this processing");
 
 
   return EXIT_SUCCESS;
