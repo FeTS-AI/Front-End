89ab365df2f5297429c217511b6f2bf88471c92c
sarthakpati <sarthak.pati@hotmail.com>
2020-12-15 14:52:37 -0500

    fixing init

--- src/applications/FeTS_CLI.cxx
+++ src/applications/FeTS_CLI.cxx
@@ -282,11 +282,12 @@ int main(int argc, char** argv)
                       std::remove(fileToOverWrite);
                       std::ofstream outfile(fileToOverWrite);
                       outfile <<
+                        "from __future__ import absolute_import\n\n"
                         "print('\nPlease cite the following paper when using nnUNet:\n" <<
                         "Fabian Isensee, Paul F.Jaeger, Simon A.A.Kohl, Jens Petersen, Klaus H. Maier-Hein. " <<
                         "nnU-Net: a self-configuring method for deep learning-based biomedical image segmentation. " <<
                         "Nat Methods (2020). https://doi.org/10.1038/s41592-020-01008-z \n" <<
-                        "If you have questions or suggestions, feel free to open an issue at https://github.com/MIC-DKFZ/nnUNet \n')\n";
+                        "If you have questions or suggestions, feel free to open an issue at https://github.com/MIC-DKFZ/nnUNet \n')\n\nfrom . import *(base)\n";
                       outfile.close();
                       nnunet_fileOverwritten = true;
                     }
