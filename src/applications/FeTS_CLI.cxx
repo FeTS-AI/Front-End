fd1dab8ed443ef8926b95af5939b98d1268a6b90
sarthakpati <sarthak.pati@hotmail.com>
2020-12-15 14:35:26 -0500

    print statement fix

--- src/applications/FeTS_CLI.cxx
+++ src/applications/FeTS_CLI.cxx
@@ -286,7 +286,7 @@ int main(int argc, char** argv)
                         "Fabian Isensee, Paul F.Jaeger, Simon A.A.Kohl, Jens Petersen, Klaus H. Maier-Hein. " <<
                         "nnU-Net: a self-configuring method for deep learning-based biomedical image segmentation. " <<
                         "Nat Methods (2020). https://doi.org/10.1038/s41592-020-01008-z \n" <<
-                        "print('If you have questions or suggestions, feel free to open an issue at https://github.com/MIC-DKFZ/nnUNet\n')\n";
+                        "If you have questions or suggestions, feel free to open an issue at https://github.com/MIC-DKFZ/nnUNet \n')\n";
                       outfile.close();
                       nnunet_fileOverwritten = true;
                     }
