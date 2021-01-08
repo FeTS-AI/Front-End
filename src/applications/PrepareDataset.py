3127f322e84f83cc3501f3ff1c78e83eafa8e70d
sarthakpati <sarthak.pati@hotmail.com>
2021-01-08 06:54:15 -0500

    api consistency with cpp

--- src/applications/PrepareDataset.py
+++ src/applications/PrepareDataset.py
@@ -5,11 +5,9 @@ from datetime import date
 
 def main():
     copyrightMessage = 'Contact: software@cbica.upenn.edu\n\n' + 'This program is NOT FDA/CE approved and NOT intended for clinical use.\nCopyright (c) ' + str(date.today().year) + ' University of Pennsylvania. All rights reserved.' 
-    parser = argparse.ArgumentParser(prog='LabelFusion', formatter_class=argparse.RawTextHelpFormatter, description = "Fusion of different labels together.\n\n" + copyrightMessage)
-    parser.add_argument('-inputs', type=str, help = 'The absolute, comma-separated paths of labels that need to be fused', required=True)
-    parser.add_argument('-classes', type=str, help = 'The expected labels; for example, for BraTS, this should be \'0,1,2,4\'', required=True)
-    parser.add_argument('-method', type=str, help = 'The method to apply; currently available: STAPLE | ITKVoting | MajorityVoting | SIMPLE', required=True)
-    parser.add_argument('-output', type=str, help = 'The output file to write the results', required=True)
+    parser = argparse.ArgumentParser(prog='PrepareDataset', formatter_class=argparse.RawTextHelpFormatter, description = "This application calls the BraTSPipeline for all input images and stores the final and intermediate files separately.\n\n" + copyrightMessage)
+    parser.add_argument('-inputCSV', type=str, help = 'The absolute, comma-separated paths of labels that need to be fused', required=True)
+    parser.add_argument('-outputDir', type=str, help = 'The output file to write the results', required=True)
 
 if __name__ == '__main__':
     main()
