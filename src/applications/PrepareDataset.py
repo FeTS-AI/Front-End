384a07d9c7d8f52a0bc992a152b6e5b13d9c99f4
sarthakpati <sarthak.pati@hotmail.com>
2023-06-19 13:17:39 -0400

    putting common dict here - wasn't able to get the relative import to work correctly

--- src/applications/PrepareDataset.py
+++ src/applications/PrepareDataset.py
@@ -8,7 +8,13 @@ import numpy as np
 from skimage.measure import label
 from copy import deepcopy
 
-from . import modality_id_dict
+# check against all these modality ID strings with extensions
+modality_id_dict = {
+    "T1": ["t1", "t1pre", "t1precontrast"],
+    "T1GD": ["t1ce", "t1gd", "t1post", "t1postcontrast", "t1gallodinium", "t1c"],
+    "T2": ["t2"],
+    "FLAIR": ["flair", "fl", "t2flair"],
+}
 
 
 def _read_image_with_min_check(filename):
