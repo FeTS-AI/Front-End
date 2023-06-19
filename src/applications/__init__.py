14371589aace4dabaedcbec8e112de288a3aa7b6
sarthakpati <sarthak.pati@hotmail.com>
2023-06-19 13:17:48 -0400

    removed

--- src/applications/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# check against all these modality ID strings with extensions
-modality_id_dict = {
-    "T1": ["t1", "t1pre", "t1precontrast"],
-    "T1GD": ["t1ce", "t1gd", "t1post", "t1postcontrast", "t1gallodinium", "t1c"],
-    "T2": ["t2"],
-    "FLAIR": ["flair", "fl", "t2flair"],
-}
