a36da5aaa2546f860ee24fd243fc3b7fa76cee46
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-06-21 11:01:23 -0500

    Reformat PrepareDataset logic for reusability

---
+++ src/mlcubes/data_preparation/project/stages/get_csv.py
@@ -0,0 +1,96 @@
+from .row_stage import RowStage
+from .CreateCSVForDICOMs import CSVCreator
+from .utils import update_row_with_dict, get_id_tp
+from pathlib import Path
+
+import pandas as pd
+from typing import Union
+import os
+import shutil
+
+
+class AddToCSV(RowStage):
+    def __init__(
+        self, input_dir: str, output_csv: str, out_dir: str, prev_stage_path: str
+    ):
+        self.input_dir = input_dir
+        self.output_csv = output_csv
+        self.out_dir = out_dir
+        self.prev_stage_path = prev_stage_path
+        os.makedirs(self.out_dir, exist_ok=True)
+        self.csv_processor = CSVCreator(self.input_dir, self.output_csv)
+        if os.path.exists(self.output_csv):
+            # Use the updated version of the CSV
+            self.contents = pd.read_csv(self.output_csv)
+            self.csv_processor.output_df_for_csv = self.contents
+        else:
+            # Use the default, empty version
+            self.contents = self.csv_processor.output_df_for_csv
+
+    def should_run(self, index: Union[str, int], report: pd.DataFrame) -> bool:
+        """Determines if getting a new CSV is necessary.
+        This is done by checking the existence of the expected file
+
+        Args:
+            index (Union[str, int]): case index in the report
+            report (pd.DataFrame): Dataframe containing the current state of the preparation flow
+
+        Returns:
+            bool: wether this stage should be executed
+        """
+        id, tp = get_id_tp(index)
+        prev_case_path = os.path.join(self.prev_stage_path, id, tp)
+
+        return os.path.exists(prev_case_path)
+
+    def execute(self, index: Union[str, int], report: pd.DataFrame) -> pd.DataFrame:
+        """Adds valid cases to the data csv that is used for later processing
+        Invalid cases are flagged in the report
+
+        Args:
+            index (Union[str, int]): case index in the report
+            report (pd.DataFrame): DataFrame containing the current state of the preparation flow
+
+        Returns:
+            pd.DataFrame: Updated report dataframe
+        """
+        id, tp = get_id_tp(index)
+        subject_path = os.path.join(self.input_dir, id)
+        tp_path = os.path.join(subject_path, tp)
+        subject_out_path = os.path.join(self.out_dir, id)
+        tp_out_path = os.path.join(subject_out_path, tp)
+        # We will first copy the timepoint to the out folder
+        # This is so, if successful, the csv will point to the data
+        # in the next stage, instead of the previous
+        shutil.copytree(tp_path, tp_out_path)
+
+        self.csv_processor.process_timepoint(tp, id, subject_out_path)
+        report_data = {
+            "status": 1,
+            "status_name": "VALIDATED",
+            "comment": "",
+            "data_path": tp_out_path,
+            "labels_path": "",
+        }
+        if self.csv_processor.subject_timepoint_missing_modalities:
+            shutil.rmtree(tp_out_path, ignore_errors=True)
+            comment = "There are missing modalities. Please check the data"
+            report_data["status"] = -1.1
+            report_data["status_name"] = "MISSING_MODALITIES"
+            report_data["data_path"] = tp_path
+            report_data["comment"] = comment
+        elif self.csv_processor.subject_timepoint_extra_modalities:
+            shutil.rmtree(tp_out_path, ignore_errors=True)
+            comment = "There are extra modalities. Please check the data"
+            report_data["status"] = -1.2
+            report_data["status_name"] = "EXTRA MODALITIES"
+            report_data["data_path"] = tp_path
+            report_data["comment"] = comment
+        else:
+            shutil.rmtree(tp_path)
+
+        update_row_with_dict(report, report_data, index)
+
+        self.csv_processor.write()
+
+        return report
---
+++ src/mlcubes/data_preparation/project/stages/nifti_transform.py
@@ -0,0 +1,126 @@
+from typing import Union
+import pandas as pd
+import os
+import shutil
+
+from .row_stage import RowStage
+from .PrepareDataset import Preparator
+from .utils import update_row_with_dict, get_id_tp
+
+
+class NIfTITransform(RowStage):
+    def __init__(self, data_csv: str, out_path: str, prev_stage_path: str):
+        self.data_csv = data_csv
+        self.out_path = out_path
+        self.prev_stage_path = prev_stage_path
+        os.makedirs(self.out_path, exist_ok=True)
+        self.prep = Preparator(data_csv, out_path)
+
+    def should_run(self, index: Union[str, int], report: pd.DataFrame) -> bool:
+        """Determine if case at given index needs to be converted to NIfTI
+
+        Args:
+            index (Union[str, int]): Case index, as used by the report dataframe
+            report (pd.DataFrame): Report Dataframe for providing additional context
+
+        Returns:
+            bool: Wether this stage should be executed for the given case
+        """
+        id, tp = get_id_tp(index)
+        prev_case_path = os.path.join(self.prev_stage_path, id, tp)
+        return os.path.exists(prev_case_path)
+
+    def execute(self, index: Union[str, int], report: pd.DataFrame) -> pd.DataFrame:
+        """Executes the NIfTI transformation stage on the given case
+
+        Args:
+            index (Union[str, int]): case index, as used by the report
+            report (pd.DataFrame): DataFrame containing the current state of the preparation flow
+
+        Returns:
+            pd.DataFrame: Updated report dataframe
+        """
+        self.__prepare_exec()
+        self.__process_case(index)
+        report = self.__update_report(index, report)
+        self.prep.write()
+
+        return report
+
+    def __prepare_exec(self):
+        # Reset the file contents for errors
+        open(self.prep.stderr_log, "w").close()
+
+        # Update the out dataframes to current state
+        self.prep.read()
+
+    def __process_case(self, index: Union[str, int]):
+        id, tp = get_id_tp(index)
+        df = self.prep.subjects_df
+        row = df[(df["SubjectID"] == id) & (df["Timepoint"] == tp)].iloc[0]
+        self.prep.process_row(row)
+
+    def __update_prev_stage_state(self, index: Union[str, int], report: pd.DataFrame):
+        prev_data_path = report.loc[index]["data_path"]
+        shutil.rmtree(prev_data_path)
+
+    def __undo_current_stage_changes(self, index: Union[str, int]):
+        id, tp = get_id_tp(index)
+        fets_path = os.path.join(self.out_path, "DataForFeTS", id, tp)
+        qc_path = os.path.join(self.out_path, "DataForQC", id, tp)
+        shutil.rmtree(fets_path, ignore_errors=True)
+        shutil.rmtree(qc_path, ignore_errors=True)
+
+    def __update_report(
+        self, index: Union[str, int], report: pd.DataFrame
+    ) -> pd.DataFrame:
+        # TODO: What should be reported? We have the processed data,
+        # QC subjects with negative intensities and
+        # QC subjects with bratspipeline error
+        id, tp = get_id_tp(index)
+        failing = self.prep.failing_subjects
+        failing_subject = failing[
+            (failing["SubjectID"] == id) & (failing["Timepoint"] == tp)
+        ]
+        if len(failing_subject):
+            report = self.__report_failure(index, report)
+            self.__undo_current_stage_changes(index)
+        else:
+            report = self.__report_success(index, report)
+            self.__update_prev_stage_state(index, report)
+
+        return report
+
+    def __report_success(
+        self, index: Union[str, int], report: pd.DataFrame
+    ) -> pd.DataFrame:
+        id, tp = get_id_tp(index)
+        fets_path = os.path.join(self.out_path, "DataForFeTS", id, tp)
+        report_data = {
+            "status": 2,
+            "status_name": "CONVERTED_TO_NIfTI",
+            "comment": "",
+            "data_path": fets_path,
+            "labels_path": "",
+        }
+        update_row_with_dict(report, report_data, index)
+        return report
+
+    def __report_failure(
+        self, index: Union[str, int], report: pd.DataFrame
+    ) -> pd.DataFrame:
+        id, tp = get_id_tp(index)
+        prev_data_path = report.loc[index]["data_path"]
+
+        with open(self.prep.stderr_log, "r") as f:
+            msg = f.read()
+
+        report_data = {
+            "status": -2,
+            "status_name": "NIfTI_CONVERSION_FAILED",
+            "comment": msg,
+            "data_path": prev_data_path,
+            "labels_path": "",
+        }
+        update_row_with_dict(report, report_data, index)
+        return report
