f55cbe5df4b6da8067e96e156e0e4625c2faac6d
sarthakpati <sarthak.pati@hotmail.com>
2023-07-13 18:00:05 -0400

    no need for this

--- .github/workflows/docker-build-command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ls -lt; ls -lt ~/work; docker run --rm --entrypoint="mkdir bin; cmake -DITK_DIR=~/work/CaPTK/bin/ITK-build -DDCMTK_DIR=~/work/CaPTK/bin/DCMTK-build -DCMAKE_INSTALL_PREFIX="./install/appdir/usr" -DBUILD_TESTING=OFF ..; make && make install/strip;" -v "$(pwd)"/Front-End:/Front-End ghcr.io/fets-ai/front-end:latest
