2ce8fa4bafbe35dbe4158d02d2eae45f6066f391
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-08-08 10:57:22 -0500

    Set backup to read-only

--- mlcubes/data_preparation/project/stages/utils.py
+++ mlcubes/data_preparation/project/stages/utils.py
@@ -10,6 +10,20 @@ def update_row_with_dict(df, d, idx):
 def get_id_tp(index: str):
     return index.split("|")
 
+
+def set_files_read_only(path):
+    for root, dirs, files in os.walk(path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            os.chmod(file_path, 0o444)  # Set read-only permission for files
+
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            set_files_read_only(
+                dir_path
+            )  # Recursively call the function for subdirectories
+
+
 class MockTqdm(tqdm):
     def __getattr__(self, attr):
         return lambda *args, **kwargs: None
