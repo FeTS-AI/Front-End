0737cb2be2e523f41b7e2cd6d4c15cafeaf87b88
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-08-31 16:02:32 -0500

    Sync data to input data. Implement confirm stage

---
+++ mlcubes/data_preparation/project/stages/confirm.py
@@ -0,0 +1,122 @@
+from typing import Union
+import os
+import yaml
+import shutil
+
+import pandas as pd
+from pandas import DataFrame
+
+from .dset_stage import DatasetStage
+from .utils import get_id_tp
+from .constants import TUMOR_MASK_FOLDER, INTERIM_FOLDER, FINAL_FOLDER
+
+
+class ConfirmStage(DatasetStage):
+    def __init__(
+        self,
+        data_csv: str,
+        out_data_path: str,
+        out_labels_path: str,
+        prev_stage_path: str,
+        backup_path: str,
+    ):
+        self.data_csv = data_csv
+        self.out_data_path = out_data_path
+        self.out_labels_path = out_labels_path
+        self.prev_stage_path = prev_stage_path
+        self.backup_path = backup_path
+        self.status_code = 7
+
+    def get_name(self):
+        return "Annotations Confirmation"
+
+    def __get_input_data_path(self, index: Union[str, int]):
+        id, tp = get_id_tp(index)
+        path = os.path.join(self.prev_stage_path, FINAL_FOLDER, id, tp)
+        return path
+
+    def __get_input_label_path(self, index: Union[str, int]):
+        id, tp = get_id_tp(index)
+        path = os.path.join(self.prev_stage_path, INTERIM_FOLDER, id, tp, "reviewed")
+        case = os.listdir(path)[0]
+
+        return os.path.join(path, case)
+
+    def __get_output_data_path(self, index: Union[str, int]):
+        id, tp = get_id_tp(index)
+        path = os.path.join(self.out_data_path, id, tp)
+        return path
+
+    def __get_output_label_path(self, index: Union[str, int]):
+        id, tp = get_id_tp(index)
+        path = os.path.join(self.out_labels_path, id, tp)
+        filename = f"{id}_{tp}_final_seg.nii.gz"
+        return os.path.join(path, filename)
+
+    def __confirm(self, exact_match_percent: float) -> bool:
+        exact_match_percent = round(exact_match_percent * 100, 2)
+        msg = (
+            f"We've identified {exact_match_percent}% of cases have not been modified "
+            + "with respect to the baseline segmentation. Do you confirm this is intended? "
+            + "[Y]/n"
+        )
+
+        # user_input = input(msg).lower()
+        user_input = "y"
+        return user_input == "y" or user_input == ""
+
+    def __report_failure(self, report: DataFrame) -> DataFrame:
+        # For this stage, failure is done when the user doesn't confirm
+        # This means he probably wants to keep working on the data
+        # And needs to know which rows are exact matches.
+        # Because of this, failing this stage keeps the report intact
+        return report
+
+    def __process_row(self, row: pd.Series) -> pd.Series:
+        """process a row by moving the required files
+        to their respective locations, and removing any extra files
+
+        Args:
+            report (DataFrame): data preparation report
+
+        Returns:
+            DataFrame: modified data preparation report
+        """
+        index = row.index
+        input_data_path = self.__get_input_data_path(index)
+        input_label_path = self.__get_input_label_path(index)
+        output_data_path = self.__get_output_data_path(index)
+        output_label_path = self.__get_output_label_path(index)
+
+        shutil.copy(input_data_path, output_data_path)
+        shutil.copy(input_label_path, output_label_path)
+
+        row["status"] = self.status_code
+        row["status_name"] = "ANNOTATION_CONFIRMED"
+        row["data_path"] = output_data_path
+        row["labels_path"] = output_label_path
+        row["comment"] = ""
+        return row
+
+    def __report_success(self, report: DataFrame) -> DataFrame:
+        pass
+
+    def should_run(self, report: DataFrame) -> bool:
+        # Should run once all cases have been compared to the ground truth
+        missing_voxels = report["num_changed_voxels"].isnull().values.any()
+
+        return not missing_voxels
+
+    def execute(self, report: DataFrame) -> DataFrame:
+        exact_match_percent = (report["num_changed_voxels"] == 0).sum() / len(report)
+        confirmed = self.__confirm(exact_match_percent)
+
+        if not confirmed:
+            return self.__report_failure(report)
+
+        report_bak = report.copy(deep=True)
+
+        report.apply(self.__process_row, axis=1)
+        print(report)
+
+        return report_bak
--- mlcubes/data_preparation/project/stages/generate_report.py
+++ mlcubes/data_preparation/project/stages/generate_report.py
@@ -2,40 +2,101 @@ from .dset_stage import DatasetStage
 import pandas as pd
 import numpy as np
 import os
+from pathlib import Path
+import hashlib
+import shutil
+
+
+# Taken from https://stackoverflow.com/questions/24937495/how-can-i-calculate-a-hash-for-a-filesystem-directory-using-python
+def md5_update_from_dir(directory, hash):
+    assert Path(directory).is_dir()
+    for path in sorted(Path(directory).iterdir(), key=lambda p: str(p).lower()):
+        hash.update(path.name.encode())
+        if path.is_file():
+            with open(path, "rb") as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash.update(chunk)
+        elif path.is_dir():
+            hash = md5_update_from_dir(path, hash)
+    return hash
+
+
+def md5_dir(directory):
+    return md5_update_from_dir(directory, hashlib.md5()).hexdigest()
 
 
 class GenerateReport(DatasetStage):
-    def __init__(self, input_path: str):
+    def __init__(self, input_path: str, output_path: str):
         self.input_path = input_path
+        self.output_path = output_path
 
     def should_run(self, report: pd.DataFrame):
-        return report is None
+        return True
 
     def execute(self, report: pd.DataFrame):
         # Rewrite the report
-        cols = ["status", "status_name", "comment", "data_path", "labels_path"]
-        report = pd.DataFrame(columns=cols)
+        cols = [
+            "status",
+            "status_name",
+            "comment",
+            "data_path",
+            "labels_path",
+            "input_hash",
+        ]
+        if report is None:
+            report = pd.DataFrame(columns=cols)
+
+        observed_cases = set()
 
         for subject in os.listdir(self.input_path):
-            subject_path = os.path.join(self.input_path, subject)
-            if not os.path.isdir(subject_path):
+            in_subject_path = os.path.join(self.input_path, subject)
+            out_subject_path = os.path.join(self.input_path, subject)
+
+            if not os.path.isdir(in_subject_path):
                 continue
 
-            for timepoint in os.listdir(subject_path):
-                tp_path = os.path.join(subject_path, timepoint)
-                if not os.path.isdir(tp_path):
+            for timepoint in os.listdir(in_subject_path):
+                in_tp_path = os.path.join(in_subject_path, timepoint)
+                out_tp_path = os.path.join(out_subject_path, timepoint)
+
+                if not os.path.isdir(in_tp_path):
                     continue
+
+                input_hash = md5_dir(in_tp_path)
+
+                index = f"{subject}|{timepoint}"
+
+                # Keep track of the cases that were found on the input folder
+                observed_cases.add(index)
+
+                if index in report.index:
+                    # Case has already been identified, see if input hash is different
+                    # if so, override the contents and restart the state for that case
+                    if report.loc[index]["input_hash"] == input_hash:
+                        continue
+
+                    shutil.rmtree(out_tp_path, ignore_errors=True)
+                    shutil.copytree(in_tp_path, out_tp_path)
+                    report = report.drop(index)
 
                 data = {
                     "status": 0,
                     "status_name": "IDENTIFIED",
                     "comment": "",
-                    "data_path": tp_path,
+                    "data_path": out_tp_path,
                     "labels_path": "",
                     "changed_voxels": np.nan,
+                    "input_hash": input_hash,
                 }
                 subject_series = pd.Series(data)
-                subject_series.name = f"{subject}|{timepoint}"
+                subject_series.name = index
                 report = report.append(subject_series)
+
+        reported_cases = set(report.index)
+        removed_cases = reported_cases - observed_cases
+
+        # Stop reporting removed cases
+        for case_index in removed_cases:
+            report = report.drop(case_index)
 
         return report
